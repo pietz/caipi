3c159b85d602ada7a22a8bb9ba269644c32a0580
Paul-Louis Pröve <mail@plpp.de>
2024-02-21 00:06:05 +0100

    Add jinja2 to requirements.txt and update appLocation in swa-cli.config.json

---
+++ api/build.py
@@ -0,0 +1,16 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
+def render(env, inp, out):
+    template = env.get_template(inp)
+    output = template.render()
+    with open(out, "w") as f:
+        f.write(output)
+
+
+# Initialize the Jinja2 environment
+env = Environment(loader=FileSystemLoader("api/templates"))
+
+render(env, "landing.html", "public/index.html")
+render(env, "dashboard.html", "public/app/index.html")
